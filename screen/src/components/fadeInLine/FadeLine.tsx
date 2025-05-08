458b7240204f31245de7b001e91078fd28d0e359
supershunan <1552736550@qq.com>
2025-05-09 00:12:33 +0800

    #feat: 增加部分大屏组件

---
+++ screen/src/components/fadeInLine/FadeLine.tsx
@@ -0,0 +1,207 @@
+import { useEffect, useRef, useState } from 'react'
+import * as echarts from 'echarts';
+
+export default function FadeLine() {
+    const chartRef = useRef<HTMLDivElement>(null);
+    const [option, setOption] = useState({
+        grid: {
+            left: "5%",
+            right: "5%",
+            top: "25%",
+            bottom: "5%",
+            width: "90%",
+            heigth: "100%",
+            containLabel: true
+        },
+        legend: {
+            top: "4%",
+            right: "5%",
+            textStyle: {
+                color: "#ffff",
+                fontSize: 12,
+                lineHeight: 20
+            },
+            icon: "circle",
+        },
+        tooltip: {
+            trigger: "axis",
+            axisPointer: {
+                type: "shadow",
+                shadowStyle: { opacity: 0.2 }
+            },
+            backgroundColor: "rgba(0,0,0,1)",
+            borderWidth: 1,
+            borderColor: "#999999",
+            textStyle: {
+                color: "#ffffff",
+                fontSize: 10
+            },
+            extraCssText: "z-index: 9999;",
+            appendToBody: true,
+            formatter: function (params: any) {
+                let result = `${params[0].axisValueLabel}<br/>`
+                params.forEach((param: any) => {
+                    const marker = param.marker
+                    const seriesName = param.seriesName
+                    const value = param.value
+                    if (param.componentSubType === "bar") {
+                        result += `${marker} ${seriesName}: ${value}<br/>`
+                    } else {
+                        result += `${marker} ${seriesName}: ${value}<br/>`
+                    }
+                })
+                return result
+            }
+        },
+        xAxis: {
+            type: "category",
+            boundaryGap: false,
+            axisLine: {
+                show: false,
+                lineStyle: {
+                    color: "#435459"
+                }
+            },
+            axisTick: {
+                show: false
+            },
+            splitLine: {
+                show: false
+            },
+            axisLabel: {
+                color: "#ffffff",
+                fontSize: 10,
+                interval: 0,
+                padding: [0, 0, 0, 0]
+            },
+            data: ["00:00", "06:00", "12:00", "18:00", "24:00"]
+        },
+        yAxis: [
+            {
+                type: "value",
+                axisLabel: {
+                    formatter: "{value}",
+                    color: "#fff"
+                }
+            },
+            // {
+            //     type: "value",
+            //     axisLabel: {
+            //         formatter: "{value}",
+            //         color: "#fff"
+            //     }
+            // }
+        ],
+        color: [
+            "rgb(21, 229, 236)", // 线上销售
+            "rgb(255, 0, 0)"  // 线下门店
+          ],
+        series: [
+            {
+                name: '同比趋势',
+                data: [16, 20, 15, 20, 15],
+                type: "line",
+                smooth: true,
+                symbol:
+                    "image://data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAAAXNSR0IArs4c6QAAAQ5JREFUOE+1la1OQ0EQRs8kCFIQdVCCxIBpCK1AInkDHC+B4QkwvASub4GsaE0NGCRpKhGFVDQZ+Mjem9vNbkphmWTMfjNnd3b2x0iYuxuwH7wNbIewBfAGzORm5nG6ElfM3QU6AXZSkzXG3oEnMxO8thWgux8DR2tAsfxiZs/VYA38Jazi1NBvYCizv+HK4vCRyrfQgIsf7Nm6+bSnjwIeAGeZaGnXQDfoE+ABmGbixwKeAoeJAMHuv3w30ubATQb6KqDKjZPEuAXOMysZAncJbS7gJbCVEAdAKwP8AK4S2vJfgMVLLt6UDtAreWx0W8od7OJXryq16OPwR2j6+WpAyz2wDagape7vARt9AZ+G3HmhiKS3xwAAAABJRU5ErkJggg==",
+                symbolSize: 10,
+                showSymbol: false, // 是否显示 symbol, 如果 false 则只有在 tooltip hover 的时候显示。
+                // yAxisIndex: 1,
+                label: {
+                    show: true,
+                    position: "top",
+                    distance: 10,
+                    color: "#ffffff",
+                    fontSize: 8
+                },
+                lineStyle: {
+                    shadowColor: "rgb(0, 0, 0, 0.4)",
+                    shadowBlur: 3,
+                    shadowOffsetY: 10,
+                    width: 3,
+                    color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [
+                        {
+                            offset: 0,
+                            color: "rgb(48, 79, 80)"
+                        },
+                        {
+                            offset: 1,
+                            color: "rgb(0, 255, 242)"
+                        }
+                    ])
+                },
+                // areaStyle: {
+                //     color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                //         {
+                //             offset: 0,
+                //             color: "rgba(25, 227, 234, 0.4)"
+                //         },
+                //         {
+                //             color: "rgba(38, 176, 192, 0)",
+                //             offset: 1
+                //         }
+                //     ])
+                // }
+            },
+            {
+                name: '流入情况',
+                data: [12, 2, 15, 20, 5],
+                type: "line",
+                smooth: true,
+                symbol:
+                    "image://data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAAAXNSR0IArs4c6QAAAQ5JREFUOE+1la1OQ0EQRs8kCFIQdVCCxIBpCK1AInkDHC+B4QkwvASub4GsaE0NGCRpKhGFVDQZ+Mjem9vNbkphmWTMfjNnd3b2x0iYuxuwH7wNbIewBfAGzORm5nG6ElfM3QU6AXZSkzXG3oEnMxO8thWgux8DR2tAsfxiZs/VYA38Jazi1NBvYCizv+HK4vCRyrfQgIsf7Nm6+bSnjwIeAGeZaGnXQDfoE+ABmGbixwKeAoeJAMHuv3w30ubATQb6KqDKjZPEuAXOMysZAncJbS7gJbCVEAdAKwP8AK4S2vJfgMVLLt6UDtAreWx0W8od7OJXryq16OPwR2j6+WpAyz2wDagape7vARt9AZ+G3HmhiKS3xwAAAABJRU5ErkJggg==",
+                symbolSize: 10,
+                showSymbol: false, // 是否显示 symbol, 如果 false 则只有在 tooltip hover 的时候显示。
+                // yAxisIndex: 1,
+                label: {
+                    show: true,
+                    position: "top",
+                    distance: 10,
+                    color: "#ffffff",
+                    fontSize: 8
+                },
+                lineStyle: {
+                    shadowColor: "rgb(0, 0, 0, 0.4)",
+                    shadowBlur: 3,
+                    shadowOffsetY: 10,
+                    width: 3,
+                    color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [
+                        {
+                            offset: 0,
+                            color: "rgb(66, 39, 39)"
+                        },
+                        {
+                            offset: 1,
+                            color: "rgb(255, 0, 0)"
+                        }
+                    ])
+                },
+                // areaStyle: {
+                //     color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                //         {
+                //             offset: 0,
+                //             color: "rgba(188, 36, 168, 0.4)"
+                //         },
+                //         {
+                //             color: "rgba(211, 22, 145, 0)",
+                //             offset: 1
+                //         }
+                //     ])
+                // }
+            }
+        ]
+    });
+
+    useEffect(() => {
+        if (chartRef.current) {
+            const chart = echarts.init(chartRef.current);
+            chart.setOption(option);
+        }
+    }, [option]);
+
+    return (
+        <div style={{ background: 'black' }}>
+            <div ref={chartRef} style={{ width: '500px', height: '300px' }}></div>
+        </div>
+    )
+}
---
+++ screen/src/components/pie/Pie.tsx
@@ -0,0 +1,136 @@
+import { useEffect, useRef, useState } from 'react'
+import * as echarts from 'echarts';
+import pieMidCircle from '../../assets/pie/pie-mid-circle.png';
+
+export default function Pie() {
+    const chartRef = useRef<HTMLDivElement>(null);
+    const [data, setData] = useState([
+        {
+            value: 25,
+            name: "体育",
+            itemStyle: {
+                //颜色渐变
+                color: new echarts.graphic.LinearGradient(0, 0, 1, 1, [
+                    { offset: 0, color: "rgba(11, 77, 44, 1)" },
+                    { offset: 1, color: "rgba(77, 255, 181, 1)" }
+                ])
+            }
+        },
+        {
+            value: 40,
+            name: "美术",
+            itemStyle: {
+                //颜色渐变
+                color: new echarts.graphic.LinearGradient(0, 0, 1, 1, [
+                    { offset: 0, color: "rgba(3,65,128,1)" },
+                    { offset: 1, color: "rgba(115,208,255,1)" }
+                ])
+            }
+        },
+        {
+            value: 20,
+            name: "数学",
+            itemStyle: {
+                //颜色渐变
+                color: new echarts.graphic.LinearGradient(0, 0, 1, 1, [
+                    { offset: 0, color: "rgba(255, 0, 0, 1)" },
+                    { offset: 1, color: "rgba(255, 100, 100, 1)" }
+                ])
+            }
+        },
+        {
+            value: 15,
+            name: "语文",
+            itemStyle: {
+                //颜色渐变
+                color: new echarts.graphic.LinearGradient(0, 0, 1, 1, [
+                    { offset: 0, color: "rgba(153, 105, 38, 1)" },
+                    { offset: 1, color: "rgba(255, 200, 89, 1)" }
+                ])
+            }
+        }
+    ])
+    const total = data.reduce((sum, item) => sum + item.value, 0);
+    const [option, setOption] = useState({
+        grid: {
+            left: "5%",
+            right: "15%",
+            top: "10%",
+            bottom: "10%",
+            containLabel: true
+        },
+        tooltip: {
+            trigger: 'item'
+        },
+        legend: {
+            type: 'scroll',
+            orient: 'vertical',
+            right: 10,
+            top: '20%',
+            bottom: 20,
+            textStyle: {
+                color: '#fff'
+            }
+        },
+        graphic: [
+            {
+                type: 'image',
+                id: 'center-img',
+                left: 'center',
+                top: 'center',
+                z: 10,
+                style: {
+                    image: pieMidCircle,
+                    width: 140,
+                    height: 140,
+                }
+            },
+            {
+                type: 'text',
+                left: 'center',
+                top: 'center',
+                z: 11,
+                style: {
+                    text: `总数：${total}`,
+                    x: 0,
+                    y: 0,
+                    textAlign: 'center',
+                    textVerticalAlign: 'middle',
+                    font: 'bold 16px Microsoft YaHei',
+                    fill: '#fff',
+                }
+            }
+        ],
+        series: [
+            {
+                type: "pie",
+                itemStyle: {
+                    borderWidth: 5,
+                    borderColor: "rgba(26, 57, 77,1)"
+                },
+                label: {
+                    show: true,
+                    position: 'outside',
+                    formatter: '{d}',
+                    color: '#fff'
+                },
+                radius: ["55%", "70%"],
+                color: ["#c487ee", "#deb140", "#49dff0", "#034079", "#6f81da", "#00ffb4"],
+                data
+            }
+        ]
+    });
+
+    useEffect(() => {
+        if (chartRef.current) {
+            const chart = echarts.init(chartRef.current);
+            chart.setOption(option);
+        }
+    }, [option]);
+
+    return (
+        <div style={{ background: 'black' }}>
+            <div ref={chartRef} style={{ width: '500px', height: '300px' }}></div>
+        </div>
+    )
+}
