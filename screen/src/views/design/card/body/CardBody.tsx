375d6045841220d58be6ea201d00f66453d46306
志强 <zhiqiang@mp.com>
2025-06-09 23:03:37 +0800

    '#style:首次加载动画'

--- screen/src/views/design/card/body/CardBody.tsx
+++ screen/src/views/design/card/body/CardBody.tsx
@@ -9,11 +9,32 @@ interface PropsImpl {
 }
 
 export default function CardBody({ direction, slot }: PropsImpl) {
+  const [show, setShow] = useState(true);
+  const [showSlot, setShowSlot] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShow(false);
+      setShowSlot(true);
+    }, 1000);
+  }, []);
   return (
     <div
       className={classNames("card_body_main", `card_body_main_${direction}`)}
     >
-      {slot}
+      {show && (
+        <div
+          className={classNames(
+            `animate__animated ${
+              direction === DirectionEnum.LEFT
+                ? "animate__fadeInLeft"
+                : "animate__fadeInRight"
+            } card_body_filter
+          `
+          )}
+        ></div>
+      )}
+      {showSlot && <>{slot}</>}
     </div>
   );
 }
